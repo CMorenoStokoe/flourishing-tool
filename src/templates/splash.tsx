5ab3a0cbb2191aaa5151b2712ecec32a95d1b604
CMorenoStokoe <cm14911@bristol.ac.uk>
2021-12-08 09:59:08 +0000

    started cover

--- src/templates/splash.tsx
+++ src/templates/splash.tsx
@@ -4,9 +4,27 @@ export function Splash(props:{
     }
 }):JSX.Element {
     return(
-        <div>
-            <h1>headline</h1>
-            <button onClick={()=>{props.onClick.start()}}>Start measure</button>
+        <div className='flex flex-col justify-center items-center'>
+            <div className='m-2 p-2'>
+                <h1 className='text-xl'>
+                    FSM Online: Assessment tool
+                </h1>
+                <p className='text-lg'>
+                    Empower students and achieve salutogenic wellbeing
+                </p>
+            </div>
+            <a 
+                className='m-2 p-2 rounded-xl bg-gray-600 text-white'
+                href='https://flourishingeducation.co.uk' target='_blank' rel='noreferrer'
+            >
+                Learn more
+            </a>          
+            <button 
+                className='m-4 p-2 rounded-xl bg-green-600 text-white'
+                onClick={()=>{props.onClick.start()}}
+            >
+                Start measure
+            </button>
         </div>
     )
 }
